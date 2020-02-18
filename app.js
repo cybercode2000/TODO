4da21dafc5c2de6e56e74dcc6c4c6a8f34032e28
cybercode2000 <damicodd123@gmail.com>
2020-02-18 15:51:29 -0500

    uploading

---
+++ app.js
@@ -0,0 +1,47 @@
+// const task = document.querySelector('input');
+// const button = document.querySelector('button');
+// const div = document.querySelector('div');
+
+
+// function fresher(){
+//     const alling = localStorage.getItem('data');
+//     const me = JSON.parse(alling);
+//     const eachTask = me.task
+//     eachTask.forEach(element => {
+//         const li = document.createElement('li');
+//         div.appendChild(li);
+//         li.innerHTML = element
+//     });
+// }
+// fresher()
+
+
+// const data = {
+//     "task": [
+
+//     ]   
+// }
+
+// button.addEventListener('click', () => {
+//     const memory = localStorage.getItem('data');
+//     if(typeof memory == null) {
+//         const mainMemory = data.task;
+//         mainMemory.push(task.value);    
+//         localStorage.setItem('data', JSON.stringify(data))
+//         alert('saved');
+//         fresher()
+//     }
+//     else {
+//         const newMemory = JSON.parse(memory);
+//         const me = newMemory.task ;
+
+//         me.push(task.value);
+//         localStorage.setItem('data', JSON.stringify(newMemory));
+//         alert('it works');
+//         const li = document.createElement('li');
+//         div.appendChild(li);
+//         li.innerHTML = task.value;
+//     }
+// })
+
+
